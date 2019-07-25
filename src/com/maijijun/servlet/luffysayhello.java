d9e29e2d9140bbbc2379685762f77407fabf1d2b
maijijun <maijijun@foxmail.com>
2019-07-26 01:48:41 +0800

    Initial commit

---
+++ src/com/maijijun/servlet/luffysayhello.java
@@ -0,0 +1,18 @@
+package com.maijijun.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/luffysayhello")
+public class luffysayhello  extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("hello,麦基俊,我们一起来学习JAVA吧！！！");
+    }
+}
---
+++ web/jsp/luffysayhello.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: maijijun
+  Date: 2019/7/26
+  Time: 1:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<script type="text/css">
+#luffy{
+        background:url("/Img/hello luffy.jpg") no-repeat;
+        height: 100%;
+        width:100%;
+        overflow: hidden;
+        background-size: 100%;
+    }
+</script>
+<head>
+    <title>Luffy Say Hello</title>
+</head>
+<body>
+   <div id="luffy"></div>
+</body>
+</html>
